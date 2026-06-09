3a22955a8bb17d14edc4d16f216a4cd9e2f02d59
Ayush Singh <ayushsingh638684@gmail.com>
2026-06-09 19:37:24 +0530

    first commit

---
+++ intro1.cpp
@@ -0,0 +1,20 @@
+// OOPS : Object-Orinted Programming System
+
+// oops was developed because of limitations were discovered in earlier approaches to programming 
+/*
+Why OOPS Was Introduced(OOPS was developed to overcome the limitations of procedural programming)
+1. Data Security (Encapsulation)
+2. Code Reusability (Inheritance)
+3. Easier Maintenance
+4. Real-World Modeling
+5. Flexibility (Polymorphism)
+
+DRAWBACKS of FUNCTIONAL / PROCEDURAL PROGRAMMING 
+1.does not model real world problem very well
+2.if new data is added, all the functions needs to be modified to access the data 
+3.Global data is accessible to all the functions
+4.No clear boundaries and well definitions of code
+5.No Modularity : functional programs can become montolithic and difficult to maintain as they grow in complexity
+
+so there was problem in exisiting paradigm 
+*/
